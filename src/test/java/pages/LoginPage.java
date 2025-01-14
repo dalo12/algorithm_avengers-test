2eab15e188c4a50a6ba7262fcbd9441673221b8c
David López <dalo_vidpez@hotmail.com.ar>
2025-01-14 13:36:27 -0300

    Created LoginPage

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void fillEmailField(String email){
+        By emailField = By.id("email");
+        driver.findElement(emailField).sendKeys(email);
+    }
+
+    public void fillPasswordField(String password){
+        By emailField = By.id("password");
+        driver.findElement(emailField).sendKeys(password);
+    }
+}
