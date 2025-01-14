90842503cd05961df4bd64abeee343e4b3f82031
David López <dalo_vidpez@hotmail.com.ar>
2025-01-14 15:35:10 -0300

    FirefoxDriver works

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -3,42 +3,50 @@ package steps;
 import io.cucumber.java.en.*;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.LoginPage;
+
+import java.time.Duration;
 
 public class LoginSteps {
     protected WebDriver driver;
     protected String URL = "http://127.0.0.1:8000/";
     protected String dashboardURL = "http://127.0.0.1:8000/dashboard";
     protected LoginPage loginPage;
+    protected Wait<WebDriver> wait;
 
     @Given("the user is on the login page")
     public void the_user_is_on_the_login_page(){
-        System.setProperty("webdriver.chrome.verboseLogging", "true");
+//        System.setProperty("webdriver.chrome.verboseLogging", "true");
         //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         //ChromeOptions options = new ChromeOptions();
         //options.setBinary("/usr/bin/chromium-browser");
 
 
 // Before initializing WebDriver in your step definitions
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.addArguments("--disable-dev-shm-usage"); // Use /tmp instead of /dev/shm
-        options.addArguments("--headless"); // Run in headless mode if no GUI is available
-        options.addArguments("--disable-gpu"); // Disable GPU rendering
-        options.addArguments("--remote-allow-origins=*"); // Handle potential CORS issues
-        options.addArguments("--disable-extensions"); // Disable extensions for stability
-        options.addArguments("--disable-software-rasterizer"); // Disable software rendering
-        options.addArguments("--disable-setuid-sandbox"); // Disable setuid sandboxing
+//        ChromeOptions options = new ChromeOptions();
+//        //options.addArguments("--no-sandbox"); // Bypass OS security model
+//        options.addArguments("--disable-dev-shm-usage"); // Use /tmp instead of /dev/shm
+//        //options.addArguments("--headless"); // Run in headless mode if no GUI is available
+//        options.addArguments("--disable-gpu"); // Disable GPU rendering
+//        options.addArguments("--remote-allow-origins=*"); // Handle potential CORS issues
+//        options.addArguments("--disable-extensions"); // Disable extensions for stability
+//        options.addArguments("--disable-software-rasterizer"); // Disable software rendering
+//        options.addArguments("--disable-setuid-sandbox"); // Disable setuid sandboxing
 
 // Pass the options to the WebDriver
-        driver = new ChromeDriver(options);
+//        driver = new ChromeDriver(options);
 
         //driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get(URL);
 
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         loginPage = new LoginPage(driver);
     }
 
